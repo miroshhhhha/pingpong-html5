595dafd8ecdc8f11140ff9d4b4fc5f96dcfa097b
miroshhhhha <mpohuy3@gmail.com>
2025-04-03 14:11:40 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Home from "./components/views/Home";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/views/Home.jsx
@@ -0,0 +1,7 @@
+import { useEffect, useState } from "react";
+import '../styles/style.css'
+
+
+export default function Home() {
+    
+}
